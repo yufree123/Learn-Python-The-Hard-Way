d463d7717ee3145b01409e6f44908c60b0d6e17b
yuhui <1292145068@qq.com>
2015-10-24 20:11:29 +0800

    two week

---
+++ yufree123/ex38.py
@@ -0,0 +1,21 @@
+ten_things="Apples Oranges Crows Telephone Light Suger"
+
+print "Wait there's not 10 things inthat list, lte's fix that."
+
+stuff=ten_things.split(' ')
+more_stuff=["Day","Nignt","Song","Frisbee","Corn","Banana","Girl","Boy"]
+
+while len(stuff)!=10:
+      next_one=more_stuff.pop()
+      print "Adding:",next_one
+      stuff.append(next_one)
+      print "There's %d items now."%len(stuff)
+print "There we go:",stuff
+
+print "Let's do some things with stuff."
+
+print stuff
+print stuff[-1]#whoa!fancy
+print stuff.pop()
+print ' '.join(stuff)#what?cool!
+print "#".join(stuff[3:5])#super stellar!
